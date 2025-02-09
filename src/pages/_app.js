146ef775017f88eeb72b5035c4e1e215e173fa59
mohanpriya20 <mohanpriyamulagapu@gmail.com>
2025-02-08 23:14:06 -0500

    Fixed font integration with next/font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,46 @@
-import "@/styles/globals.css"; // Global CSS styles
+import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Roboto, Mina, Smooch_Sans, Merriweather, Metal, Moon_Dance } from 'next/font/google';
+
+// Load fonts with next/font
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+const mina = Mina({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+const smoochSans = Smooch_Sans({
+  weight: ['100', '900'],
+  subsets: ['latin'],
+});
+
+const merriweather = Merriweather({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+const metal = Metal({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+const moonDance = Moon_Dance({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <main className={`${roboto.variable} ${mina.variable} ${smoochSans.variable} ${merriweather.variable} ${metal.variable} ${moonDance.variable}`}>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </main>
   );
 }
